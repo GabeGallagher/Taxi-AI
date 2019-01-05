c65290b20e15a2b3642d260d0ded735329ec35e9
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2019-01-04 22:50:02 -0600

    finished implementing q-learning algorithm and solving environment in under 1000 episodes

--- Taxi.py
+++ Taxi.py
@@ -22,8 +22,9 @@ EPSILON_TAPER = 0.01
 # Get action based on epsilon greedy
 def get_action(state, e=EPSILON_START):
     # if we decide to explore or are new to this state, take a random action
-    if random.random() < e or math.isclose(max(q_table[state]), 0, rel_tol=1e-9):
-        return np.random.choice(q_table[state])
+    #if random.random() < e or math.isclose(max(q_table[state]), 0, rel_tol=1e-9):
+    if random.random() < e:
+        return random.randint(0, len(q_table[state]) - 1)
 
     return np.argmax(q_table[state])
 
@@ -36,31 +37,72 @@ def update_q(state, action, a, reward, new_state):
     q_table[state][action] += a * (reward + GAMMA * max(q_table[new_state]) - q_table[state][action])
 
 
-def update_memory(state):
-    if state not in count_table:
-        count_table[state] = 1
-    else:
-        count_table[state] += 1
+# def update_memory(state):
+#     if state not in count_table:
+#         count_table[state] = 1
+#     else:
+#         count_table[state] += 1
 
 
-def play():
+def learn():
     state = GAME.reset()
     is_done = False
-    total_reward = 0
     moves = 0
+    did_win = False
 
     while not is_done and moves < MAX_STEPS:
         action = get_action(state)
-        new_state, is_done, reward, prob = GAME.step(action)
+        new_state, reward, is_done, prob = GAME.step(action)
+        if reward == 20:
+            did_win = True
         update_q(state, action, get_alpha(), reward, new_state)
-        # update_memory(state)
-        total_reward += reward
+        state = new_state
         moves += 1
+
+    if did_win:
+        return 1
+
+    return 0
+
+
+def play():
+    success = False
+    is_done = False
+    moves = 0
+    state = GAME.reset()
+
+    while not is_done and moves < MAX_STEPS:
+        new_state, reward, is_done, prob = GAME.step(get_action(state))
+        if reward == 20:
+            success = True
+        state = new_state
+        moves += 1
+
+        if plays == 0:
+            GAME.render()
+
+    return success
 
 
 if __name__ == "__main__":
     q_table = np.zeros((STATE_SPACE, ACTION_SPACE))
-    count_table = {}
+    wins = 0
+    thousands = 1
 
     for episode in range(NUM_EPISODES):
-        play()
+        wins += learn()
+        if (episode + 1) % (1000 * thousands) == 0:
+            print("success rate: ", wins / (episode + 1))
+            print("Total Wins: ", wins)
+            print("Episodes: ", episode + 1)
+            thousands += 1
+            plays = 0
+            passed = True
+            # play the game ten times or to failure
+            while plays < 10 and passed:
+                passed = play()
+                plays += 1
+
+            if passed:
+                print("Episodes to convergence: ", episode + 1)
+                episode = NUM_EPISODES
